ec1b8fd214f8dd86ebdeb06055b44ed2051dbbd2
Alejo&Nidis <batory182@gmail.com>
2023-05-27 09:25:12 -0500

    CAmbios

---
+++ Business/Entitites/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entitites
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string TypeCurrency { get; set; }
+        public float Convertion { get; set; }
+    }
+}
--- Business/Entitites/Journey.cs
+++ Business/Entitites/Journey.cs
@@ -6,5 +6,6 @@
         public string Origin { get; set; }
         public string Destination { get; set; }
         public double Price { get; set; }
+        public int TypeTrip { get; set; }
     }
 }
