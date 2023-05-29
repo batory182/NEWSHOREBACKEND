240dcf7ce8aa77a3011c0c84bab1064a768a7621
Alejo&Nidis <batory182@gmail.com>
2023-05-28 19:48:55 -0500

    CAMBIOS

--- Domain/Services/Flight/FlightService.cs
+++ Domain/Services/Flight/FlightService.cs
@@ -2,6 +2,7 @@
 using Business.Entitites;
 using BusinessEntities.DTOs;
 using Infrastructure.ExternalWebServices;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace Domain.Services
@@ -20,23 +21,25 @@ namespace Domain.Services
             Journey journey = new Journey();
             journey.Origin = origin;
             journey.Destination = destination;
-            var flights = await _consumeRepository.GetFlightsAsync("0");
-            var routes = GetRoutes(origin, destination, flights);
+            var type = "1";
+            var flights = await _consumeRepository.GetFlightsAsync(type);
+            var routes = GetRoutes1(origin, destination, flights);
             journey.Flights = routes;
-            journey.Price = journey.Flights.Sum(x => x.Price);
+            if (journey.Flights.Count() > 0)
+                journey.Price = journey.Flights.Sum(x => x.Price);
             return journey;
         }
         private List<Flight> GetRoutes(string origin, string destination, List<Flight> flights)
         {
             List<Flight> routes = new List<Flight>();
             var origins = flights.Where(x => x.Origin == origin);
-            if(origins.Where(x => x.Destination == destination).Count() == 1 )
+            if (origins.Where(x => x.Destination == destination).Count() == 1)
             {
                 routes.Add(origins.Where(x => x.Destination == destination).First());
                 return routes;
             }
 
-            foreach (var item in origins) 
+            foreach (var item in origins)
             {
                 routes.Add(item);
                 var arrives = flights.Where(x => x.Origin == item.Destination);
@@ -46,14 +49,47 @@ namespace Domain.Services
                     {
                         routes.Add(arrive);
                     }
-                    else 
+                    else
                     {
                         routes.Remove(item);
                     }
-                }               
+                }
             }
             return routes;
         }
-
+        private List<Flight> GetRoutes1(string origin, string destination, List<Flight> flights)
+        {
+            List<Flight> routes = new List<Flight>();
+            var origins = flights.Where(x => x.Origin == origin);
+            foreach (var item in origins)
+            {
+                routes = GetRoute(origin, destination, routes, flights, item);
+                if (routes.First().Origin == origin && routes.Last().Destination == destination)
+                {
+                    return routes;
+                }
+            }
+            return new List<Flight>();
+        }
+        private List<Flight> GetRoute(string origin, string destination, List<Flight>? routes, List<Flight> flights, Flight lastRoute)
+        {
+            routes.Add(lastRoute);
+            if (routes.First().Origin == origin && routes.Last().Destination == destination)
+            {
+                return routes;
+            }
+            else
+            {
+                var arrives = flights.Where(x => x.Origin == lastRoute.Destination);
+                foreach (var arrive in arrives)
+                {
+                    if (arrive.Destination == destination)
+                    {
+                        routes = GetRoute(origin, destination, routes, flights, arrive);
+                    }
+                }
+            }
+            return routes;
+        }
     }
 }
